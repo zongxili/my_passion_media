9c154e1d822429ea51747a76dac108d0fa104963
Zongxi Li <zongxili@Zongxis-MBP.hitronhub.home>
2020-01-29 23:32:14 -0800

    added q1.php

---
+++ home_asg1/q1.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$inputArr = array(8,5,6,9,3,8,2,4,6,10,8,5,6,1,7,10,5,3,7,6); 
+// sort the array
+
+
+// create 2 empty answer arrays and a flag
+
+$ansArrayOne = array();
+
+$ansArrayTwo = array();
+
+print_r ($inputArr);
+
+$flag = -1;
+
+// take the first one to the 1st array 
+for ($i = 0; $i <= 10; $i++) {
+  echo "The number is: $x <br>";
+}
+
+// 
+?>
