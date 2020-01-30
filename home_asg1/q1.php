ca2bf0c7fefcc5e49a8ca7150f055b6fd049913c
Zongxi Li <zongxili@Zongxis-MBP.hitronhub.home>
2020-01-29 23:58:46 -0800

    finished the code for q1

--- home_asg1/q1.php
+++ home_asg1/q1.php
@@ -6,19 +6,29 @@ $inputArr = array(8,5,6,9,3,8,2,4,6,10,8,5,6,1,7,10,5,3,7,6);
 
 
 // create 2 empty answer arrays and a flag
+rsort($inputArr);
 
 $ansArrayOne = array();
 
 $ansArrayTwo = array();
 
-print_r ($inputArr);
+print_r($inputArr);
 
-$flag = -1;
+$flag = 1;
 
 // take the first one to the 1st array 
-for ($i = 0; $i <= 10; $i++) {
-  echo "The number is: $x <br>";
+for ($i = 0; $i < count($inputArr); $i = $i +2) {
+  if ($flag === 1) {
+    array_push($ansArrayOne, $inputArr[$i]);
+    if ($inputArr[$i + 1]) array_push($ansArrayTwo, $inputArr[$i + 1]);
+    $flag = $flag * -1;
+  } else {
+    array_push($ansArrayTwo, $inputArr[$i]);
+    if ($inputArr[$i + 1]) array_push($ansArrayOne, $inputArr[$i + 1]);
+    $flag = $flag * -1;
+    }
 }
 
-// 
+print_r($ansArrayOne);
+
 ?>
